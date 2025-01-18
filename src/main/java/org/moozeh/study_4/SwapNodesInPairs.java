8bcfc9c05b5ee955418c58a6dd324abb55073fb6
Chanwoo Kim <kcw2205@naver.com>
2025-01-19 00:16:21 +0900

    24. Swap Nodes in Pairs
    https://leetcode.com/problems/swap-nodes-in-pairs/description/

---
+++ src/main/java/org/moozeh/study_4/SwapNodesInPairs.java
@@ -0,0 +1,46 @@
+package org.moozeh.study_4;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        var prev = head; // simply references for linking
+        var current = head;
+        var next = head.next;
+
+        // swap
+        current.next = next.next;
+        next.next = current;
+        head = next;
+
+        prev = head.next;
+        current = prev.next;
+        if (current == null) return head;
+        next = current.next;
+
+        while(current != null && next != null) {
+            // swap
+            current.next = next.next;
+            next.next = current;
+            prev.next = next;
+
+            prev = prev.next.next;
+            current = prev.next;
+            if (current == null) break;
+            next = current.next;
+
+        }
+
+        return head;
+    }
+}
